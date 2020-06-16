85afbe2dc8fbce2e1cc127fed229fe3aa03434cf
Jessica Teekasingh <jessi@Jessicas-MBP.attlocal.net>
2020-06-16 18:44:02 -0400

    CRUD complete

--- public/css_JS/app.js
+++ public/css_JS/app.js
@@ -1,6 +1,7 @@
 const app = angular.module('CandleApp', [])
 
 app.controller('MyController', ['$http', function ($http) {
+  this.indexToShow = null
   this.createForm = {}
 
   this.createCandle = () => {
@@ -46,6 +47,28 @@ app.controller('MyController', ['$http', function ($http) {
       console.log(error);
     })
   }//deleteCandle end
+
+  this.updateCandle = (candle) => {
+    $http(
+      {
+        method: 'PUT',
+        url: '/candles/' + candle._id,
+        data: {
+          name: this.name,
+          price: this.price,
+          brand: this.brand,
+          likes: this.likes,
+          inStock: this.inStock,
+          img: this.url
+        }
+      }
+    ).then((response) => {
+      console.log(response.data);
+      this.getCandles()
+    }, (error) => {
+      console.log(error);
+    })
+  }
 
   this.getCandles()
 
