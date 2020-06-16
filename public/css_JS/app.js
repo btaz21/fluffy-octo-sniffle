a345daa97b6d0e42adc1bf71c34ce237365fd1ef
Jessica Teekasingh <jessi@Jessicas-MBP.attlocal.net>
2020-06-16 17:38:08 -0400

    set up GET and POST routes

--- public/css_JS/app.js
+++ public/css_JS/app.js
@@ -1,7 +1,36 @@
 const app = angular.module('CandleApp', [])
 
 app.controller('MyController', ['$http', function ($http) {
+  this.createForm = {}
 
+  this.createCandle = () => {
+    $http(
+      {
+        method:'POST',
+        url:'/candles',
+        data: this.createForm
+      }
+    ).then((response) => {
+      console.log(response.data);
+      this.createForm = {}
+    }, (error) => {
+      console.log(error);
+    })
+  }//createCandle end
+
+
+  this.getCandles = () => {
+    $http(
+      {
+        method:'GET',
+        url: '/candles'
+      }
+    ).then((response) => {
+      this.candles = response.data
+    }, (error) => {
+      console.log(error);
+    })
+  }//getCandles end
 
 
 }])
