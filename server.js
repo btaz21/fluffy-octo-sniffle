7dc70549ef8c936b517936ca5779eb39f6c6a171
Jessica Teekasingh <jessi@Jessicas-MBP.attlocal.net>
2020-06-16 16:59:06 -0400

    set up server.js

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
+const db = mongoose.connection
+
+// Middleware
+app.use(express.json())// returns middleware that only parses JSON
+app.use(express.static('public'))
+
+// Connect to Mongo
+mongoose.connect('mongodb://localhost:27017/luxurycandleshop', { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true },
+  () => console.log('MongoDB connection established:', 'mongodb://localhost:27017/luxurycandleshop')
+)
+
+// Error / Disconnection
+db.on('error', err => console.log(err.message + ' is Mongod not running?'))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
 
 app.get('/', (req, res) => {
   res.send('hello world')
