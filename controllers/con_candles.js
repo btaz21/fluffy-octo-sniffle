1373e0c81b02bfd41974d4d06511ed813a210d7b
Jessica Teekasingh <jessi@Jessicas-MBP.attlocal.net>
2020-06-16 17:30:51 -0400

    setup index and create routes in mongoose

--- controllers/con_candles.js
+++ controllers/con_candles.js
@@ -3,5 +3,19 @@ const router = express.Router()
 const Candles = require('../models/candles.js')
 
 
+//INDEX
+router.get('/', (req, res) => {
+  Candles.find({}, (error, foundCandle) => {
+    res.json(foundCandle)
+  })
+})
+
+
+//CREATE
+router.post('/', (req, res) => {
+  Candles.create(req.body, (error, createdCandle) => {
+    res.json(createdCandle)
+  })
+})
 
 module.exports = router
